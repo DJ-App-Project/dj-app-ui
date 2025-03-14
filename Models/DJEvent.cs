91b6cc21e2996706c61a64085e9da6e8cd035dfa
Tadej Fakin <tadej.fakin@gmail.com>
2025-03-14 21:46:28 +0100

    Edited Event class and Created MusicConfig class
    Edited Event class and Created MusicConfig class

--- Models/DJEvent.cs
+++ Models/DJEvent.cs
@@ -1,26 +1,63 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using DJ.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Text.Json.Serialization;
 
 namespace Dj.Models
 {
     public class DJEvent
     {
-        public string ID { get; set; }
-        public string DJID { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public DateTime Date { get; set; }
-        public string Location { get; set; }
-        public string QRCode { get; set; }
+        [JsonPropertyName("objectId")]
+        public string? ObjectId { get; set; }
 
-        public DJEvent(string iD, string dJID, string name, string description, DateTime date, string location, string qRCode)
+        [JsonPropertyName("id")]
+        public string? UserID { get; set; }
+
+        [JsonPropertyName("djId")]
+        public string? DJID { get; set; }
+
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string? Description { get; set; }
+
+        [JsonPropertyName("date")]
+        public DateTime Date { get; set; }
+
+        [JsonPropertyName("location")]
+        public string? Location { get; set; }
+
+        [JsonPropertyName("qrCodeText")]
+        public string? QRCode { get; set; }
+
+        [JsonPropertyName("active")]
+        public bool Active { get; set; } = true;
+
+        [JsonPropertyName("musicConfig")]
+        public MusicConfig? MusicConfig { get; set; }
+
+        public DJEvent(string objectId, string userId, string djId, string name, string description, DateTime date, string location, string qrCode, bool active, MusicConfig musicConfig)
         {
-            ID = iD;
-            DJID = dJID;
+            ObjectId = objectId;
+            UserID = userId;
+            DJID = djId;
             Name = name;
             Description = description;
             Date = date;
             Location = location;
-            QRCode = qRCode;
+            QRCode = qrCode;
+            Active = active;
+            MusicConfig = musicConfig;
+        }
+
+        public DJEvent(string name, string description, DateTime date, string location)
+        {
+            Name = name;
+            Description = description;
+            Date = date;
+            Location = location;
         }
     }
+
 }
